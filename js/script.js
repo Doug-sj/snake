04ae084729bf2af1828250e04a0a95b54304f850
Douglas Serpa Jaques <doug.sj82@gmail.com>
2020-05-26 22:10:28 -0300

    Movimentos captados do EventKeyDown KeyCode

--- js/script.js
+++ js/script.js
@@ -21,6 +21,15 @@ function criarCobrinha(){
 	}
 }
 
+document.addEventListener('keydown', update);
+
+function update(event){
+	if (event.keyCode == 37 && direction != "right") direction = "left";
+	if (event.keyCode == 38 && direction != "down") direction = "up";
+	if (event.keyCode == 39 && direction != "left") direction = "right";
+	if (event.keyCode == 40 && direction != "up") direction = "down";
+}
+
 function iniciarJogo() {
 	criarBG();
 	criarCobrinha();
@@ -34,12 +43,12 @@ function iniciarJogo() {
 
 	snake.pop();
 	let newHead = {
-		x: snakeX;
-		y: snakeY;		
+		x: snakeX,
+		y: snakeY		
 	}
 
 	snake.unshift(newHead);
-	
+
 }
 
 let jogo = setInterval(iniciarJogo, 100);
